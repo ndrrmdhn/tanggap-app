a011da0e36f1fe9cbbdac6b2b83f28e097b7974f
ndrrmdhn <andreramadhani98@gmail.com>
2024-05-16 11:32:46 +0700

    Make Footer and add stepper

---
+++ src/components/common/footer.jsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+import { Input } from "../ui/input";
+
+const Footer = () => {
+  return (
+    <div className="bg-color-2 w-full">
+      <div className="max-w-[1200px] py-[50px]     mx-auto">
+        <div className="grid grid-cols-5 text-white">
+          <div className="text-white flex  flex-col justify-center items-center w-max">
+            <img src="/images/brand-logo.svg" alt="" srcSet="" className="h-[100px] w-[100px]" />
+            <h1 className="text-2xl font-bold ">TANGGAP</h1>
+          </div>
+          <div className="space-y-5">
+            <h1 className="font-bold">Layanan Kami</h1>
+            <div className="flex flex-col">
+              <Link to={"/bantuan"}>Bantuan</Link>
+              <Link to={"/notifikasi"}>Notifikasi</Link>
+              <Link to={"/contact-us"}>Kontak Kami</Link>
+            </div>
+          </div>
+          <div className="space-y-5">
+            <h1 className="font-bold">Kategori</h1>
+            <div>
+              <Link to={"/sosial-media"}>Sosial</Link>
+            </div>
+          </div>
+          <div className="space-y-5">
+            <h1 className="font-bold">Fitur</h1>
+            <div className="flex flex-col">
+              <Link to={"/pengaduan"}>Pengaduan</Link>
+              <Link to={"/berita"}>Berita</Link>
+              <Link to={"/profile"}>Profile</Link>
+            </div>
+          </div>
+          <div className="space-y-5">
+            <h1 className="font-bold">Cari</h1>
+            <div>
+              <Input placeholder="Cari" />
+            </div>
+          </div>
+          <div className="col-start-2">
+            <h1>Tentang Aplikasi</h1>
+          </div>
+          <div className="">
+            <h1>Hubungi Kami</h1>
+          </div>
+          <div className="">
+            <h1>Kebijakan Privasi</h1>
+          </div>
+          <div className="col-start-2 mt-[30px] col-span-3  flex justify-center">
+            <h1>© 2024 Jenderal Soedirman. All Rights Reserved</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/pengaduan-page.jsx
+++ src/pages/pengaduan-page.jsx
@@ -1,9 +1,127 @@
 import AppLayout from "@/layouts/app-layout";
 
+import { useEffect, useRef, useState } from "react";
+
+const CHECKOUT_STEPS = [
+  {
+    name: "Pilih Kategori",
+    Component: () => <div>Provide your contact details.</div>,
+  },
+  {
+    name: "Tulis Pengaduan",
+    Component: () => <div>Enter your shipping address.</div>,
+  },
+  {
+    name: "Pratinjau",
+    Component: () => <div>Complete payment for your order.</div>,
+  },
+];
+
+const CheckoutStepper = ({ stepsConfig = [] }) => {
+  const [currentStep, setCurrentStep] = useState(1);
+  const [isComplete, setIsComplete] = useState(false);
+  const [margins, setMargins] = useState({
+    marginLeft: 0,
+    marginRight: 0,
+  });
+  const stepRef = useRef([]);
+
+  useEffect(() => {
+    setMargins({
+      marginLeft: stepRef.current[0].offsetWidth / 2,
+      marginRight: stepRef.current[stepsConfig.length - 1].offsetWidth / 2,
+    });
+  }, [stepRef, stepsConfig.length]);
+
+  if (!stepsConfig.length) {
+    return <></>;
+  }
+
+  console.log("MARGIN: ", margins);
+
+  const handleNext = () => {
+    setCurrentStep((prevStep) => {
+      if (prevStep === stepsConfig.length) {
+        setIsComplete(true);
+        return prevStep;
+      } else {
+        return prevStep + 1;
+      }
+    });
+  };
+
+  const calculateProgressBarWidth = () => {
+    console.log(((currentStep - 1) / (stepsConfig.length - 1)) * 100);
+    return ((currentStep - 1) / (stepsConfig.length - 1)) * 100;
+  };
+
+  const ActiveComponent = stepsConfig[currentStep - 1]?.Component;
+
+  return (
+    <>
+      <div className="stepper flex-shrink-0">
+        {stepsConfig.map((step, index) => {
+          return (
+            <div
+              key={step.name}
+              ref={(el) => (stepRef.current[index] = el)}
+              className={`step ${currentStep > index + 1 || isComplete ? "complete" : ""} ${
+                currentStep === index + 1 ? "active" : ""
+              } `}
+            >
+              <div className="step-number">{currentStep > index + 1 || isComplete ? <span>&#10003;</span> : index + 1}</div>
+              <div className="step-name">{step.name}</div>
+            </div>
+          );
+        })}
+
+        <div
+          className="progress-bar"
+          style={{
+            width: `calc(100% - ${margins.marginLeft + margins.marginRight}px)`,
+            marginLeft: margins.marginLeft,
+            marginRight: margins.marginRight,
+          }}
+        >
+          <div className="progress" style={{ width: `${calculateProgressBarWidth()}%` }}></div>
+        </div>
+      </div>
+
+      <ActiveComponent />
+
+      {!isComplete && (
+        <button className="btn" onClick={handleNext}>
+          {currentStep === stepsConfig.length ? "Finish" : "Next"}
+        </button>
+      )}
+    </>
+  );
+};
+
 const PengaduanPage = () => {
   return (
     <AppLayout>
-      <div>PengaduanPage</div>;
+      <div className="max-w-[1200px] mx-auto pt-[60px]">
+        <h1 className="text-xl font-bold">Pengaduan</h1>
+
+        <CheckoutStepper stepsConfig={CHECKOUT_STEPS} />
+        {/* <div className="h-[120px] w-full mt-[20px] rounded-[10px] shadow-xl shadow-[rgba(0,_0,_0,_0.25)] gap-5 flex border items-center justify-between px-[60px]">
+          <div className="flex flex-col flex-shrink-0 justify-center items-center">
+            <div className="w-7 h-7 rounded-full bg-slate-700"></div>
+            <p>Pilih Kategori</p>
+          </div>
+          <div className="w-full h-[1px] bg-black"></div>
+          <div className="flex flex-col flex-shrink-0 justify-center items-center">
+            <div className="w-7 h-7 rounded-full bg-slate-700"></div>
+            <p>Tulis Pengaduan</p>
+          </div>
+          <div className="w-full h-[1px] bg-black"></div>
+          <div className="flex flex-col flex-shrink-0 justify-center items-center">
+            <div className="w-7 h-7 rounded-full bg-slate-700"></div>
+            <p>Pratinjau</p>
+          </div>
+        </div> */}
+      </div>
     </AppLayout>
   );
 };
